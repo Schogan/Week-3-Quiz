99605293c0f614ade6c36509507ee076b5d12cc3
Schogan <lrschanbacher@gmail.com>
2015-06-29 18:42:10 -0500

    week 3 quiz

---
+++ application.js
@@ -0,0 +1,15 @@
+function processForm() {
+	
+		var Itsabox;
+		var content;
+		
+		
+		Itsabox = document.getElementById("Itsabox");
+		content = document.getElementById("output")
+		
+		content.innerText = "Here is the thing you asked for: " + Itsabox.value
+
+
+
+
+}
